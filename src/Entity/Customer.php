e38a5d1de76a6a13a670deae26d7a8df8c8744b6
Murat49370 <murat49370@gmail.com>
2020-12-28 11:03:59 +0100

    create all entities

---
+++ src/Entity/Customer.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ */
+class Customer
+{
+    /**
+     * @var int|null
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $email;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $firstName;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $lastName;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private ?DateTimeInterface $registeredAt;
+
+    /**
+     * @var User
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private User $user;
+
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return $this
+     */
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface|null
+     */
+    public function getRegisteredAt(): ?DateTimeInterface
+    {
+        return $this->registeredAt;
+    }
+
+    /**
+     * @param DateTimeInterface $registeredAt
+     * @return $this
+     */
+    public function setRegisteredAt(DateTimeInterface $registeredAt): self
+    {
+        $this->registeredAt = $registeredAt;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+
+}
